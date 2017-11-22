326480fdc06db519b21a73b2f41791df701252d1
quar17esma <quar17esma@gmail.com>
2017-11-22 03:08:39 +0200

    go to annotations

--- src/main/java/com/mkyong/App.java
+++ src/main/java/com/mkyong/App.java
@@ -5,20 +5,26 @@ import org.hibernate.Session;
 import com.mkyong.util.HibernateUtil;
 import com.mkyong.user.DBUser;
 
+import javax.transaction.UserTransaction;
+
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Maven + Hibernate + Oracle");
+
+		HibernateUtil.createSessionFactory();
 		Session session = HibernateUtil.getSessionFactory().openSession();
 
 		session.beginTransaction();
 		DBUser user = new DBUser();
 
-		user.setUserId(100);
 		user.setUsername("superman");
 		user.setCreatedBy("system");
-		user.setCreatedDate(new Date());
 
 		session.save(user);
 		session.getTransaction().commit();
+
+        System.out.println(user);
+
+        HibernateUtil.shutdown();
 	}
 }
--- src/main/java/com/mkyong/util/HibernateUtil.java
+++ src/main/java/com/mkyong/util/HibernateUtil.java
@@ -1,30 +1,32 @@
 package com.mkyong.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 public class HibernateUtil {
 
-	private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
 
-	private static SessionFactory buildSessionFactory() {
-		try {
-			// Create the SessionFactory from hibernate.cfg.xml
-			return new Configuration().configure().buildSessionFactory();
-		} catch (Throwable ex) {
-			// Make sure you log the exception, as it might be swallowed
-			System.err.println("Initial SessionFactory creation failed." + ex);
-			throw new ExceptionInInitializerError(ex);
-		}
-	}
+    public static SessionFactory createSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties())
+                .build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        return sessionFactory;
+    }
 
 	public static SessionFactory getSessionFactory() {
 		return sessionFactory;
 	}
 
 	public static void shutdown() {
-		// Close caches and connection pools
-		getSessionFactory().close();
+		sessionFactory.close();
 	}
 
 }
