db5a7aaf6d9b4c179a0e0aa9c2a5e856da936157
quar17esma <quar17esma@gmail.com>
2017-12-04 16:22:32 +0200

    added Picture to DB and back example

---
+++ src/main/java/com/mkyong/AvatarToDB.java
@@ -0,0 +1,58 @@
+package com.mkyong;
+
+import com.mkyong.avatar.Avatar;
+import com.mkyong.util.HibernateUtil;
+import org.hibernate.Session;
+
+import java.io.*;
+
+public class AvatarToDB {
+    private static final String PATH_IMAGE_TO_DB =
+            "C:\\Dell\\Java Projects\\Maven3-Hibernate3.6-Oracle11-Example\\HibernateExample\\src\\main\\" +
+                    "resources\\pictures\\avatar.jpg";
+    private static final String PATH_IMAGE_FROM_DB =
+            "C:\\Dell\\Java Projects\\Maven3-Hibernate3.6-Oracle11-Example\\HibernateExample\\src\\main\\" +
+                    "resources\\pictures\\avatarFromDB.jpg";
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hibernate save image into database");
+        HibernateUtil.createSessionFactory();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        int avatarId = saveImageToDB(session);
+
+        getImageFromDB(session, avatarId);
+
+        session.getTransaction().commit();
+    }
+
+    private static int saveImageToDB(Session session) throws IOException {
+
+        File file = new File(PATH_IMAGE_TO_DB);
+        int fileLength = (int) file.length();
+        byte[] bFile = new byte[fileLength];
+
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            fileInputStream.read(bFile);
+        }
+
+        Avatar avatarToDB = new Avatar();
+        avatarToDB.setImage(bFile);
+
+        session.save(avatarToDB);
+
+        return avatarToDB.getAvatarId();
+    }
+
+    private static void getImageFromDB(Session session, int avatarId) throws IOException {
+
+        Avatar avatarFromDB = (Avatar) session.get(Avatar.class, avatarId);
+        byte[] bAvatar = avatarFromDB.getImage();
+
+        try (FileOutputStream fos = new FileOutputStream(PATH_IMAGE_FROM_DB)) {
+            fos.write(bAvatar);
+        }
+    }
+}
---
+++ src/main/java/com/mkyong/avatar/Avatar.java
@@ -0,0 +1,39 @@
+package com.mkyong.avatar;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "avatar")
+public class Avatar implements Serializable {
+
+    private Integer avatarId;
+    private byte[] image;
+
+    public Avatar() {
+    }
+
+    public Avatar(byte[] image) {
+        this.image = image;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "AVATAR_ID", nullable = false, unique = true)
+    public Integer getAvatarId() {
+        return this.avatarId;
+    }
+
+    public void setAvatarId(Integer avatarId) {
+        this.avatarId = avatarId;
+    }
+
+    @Column(name = "IMAGE", nullable = false)
+    public byte[] getImage() {
+        return this.image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
